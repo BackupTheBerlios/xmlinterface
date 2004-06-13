19b41b5412d6495de749b30208a6029d6ffe26d7
dukerw <dukerw>
2004-06-13 12:43:52 +0000

    Storemethode umfasst jetzt alle zuunterstützenden GUI-Elemente.

--- xmlinterface/sebastian/src/de/fhtw/xgl/example/TestStoreClass.java
+++ xmlinterface/sebastian/src/de/fhtw/xgl/example/TestStoreClass.java
@@ -4,7 +4,9 @@
  */
 package de.fhtw.xgl.example;
 
-import de.fhtw.xgl.interpreter.swing.*;
+import de.fhtw.xgl.interpreter.swing.widgets.SwingWindow;
+import de.fhtw.xgl.interpreter.swing.widgets.SwingButton;
+import de.fhtw.xgl.interpreter.swing.widgets.SwingTextfield;
 
 // XML-Output
 import org.w3c.dom.Document;
@@ -24,14 +26,14 @@ public class TestStoreClass
 	
 	public static void main(String[] args)
 	{
-		Window sww = new Window();
+		SwingWindow sww = new SwingWindow();
 		sww.setTitle("Test");
 		sww.setSize(800, 600);
 		sww.setLocation(100, 100);
-		Textfield t = new Textfield("Testtext");
+		SwingTextfield t = new SwingTextfield("Testtext");
 		t.setSize(780, 300);
 		t.setLocation(1, 1);
-		Button b = new Button("Button1");
+		SwingButton b = new SwingButton("Button1");
 		b.setLocation(1, 305);
 		b.setSize(100, 20);
 		sww.getContentPane().add(t);
