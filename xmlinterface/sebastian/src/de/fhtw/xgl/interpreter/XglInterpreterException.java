9a5e3d97dcf1f8cb6701c20df630c8a4af3f50e5
dukerw <dukerw>
2004-06-13 12:40:44 +0000

    Kommentar erweitert

--- xmlinterface/sebastian/src/de/fhtw/xgl/interpreter/XglInterpreterException.java
+++ xmlinterface/sebastian/src/de/fhtw/xgl/interpreter/XglInterpreterException.java
@@ -5,12 +5,21 @@
 package de.fhtw.xgl.interpreter;
 
 /**
- * @author Administrator
+ * Indicates that an Exception appeared during the parsing of the XML-structure.
+ * 
+ * Mostly this Exception is caused by a wrong XML-structure. 
+ * 
+ * @author Sebastian Heide
  *
  */
 public class XglInterpreterException extends Exception
 {
 	
+	/**
+	 * Constructs an XglInterpreterException with the message msg.
+	 * 
+	 * @param msg error-message to display
+	 */
 	public XglInterpreterException(String msg)
 	{
 		super(msg);
