6365801511720096f50275c2fcc8b02d5c5f7789
dukerw <dukerw>
2004-06-13 12:39:14 +0000

    Interpreter an geändertes CallbackHandling angepasst (Callback laufen jetzt über eine globale Callbackmethode)

--- xmlinterface/sebastian/src/de/fhtw/xgl/interpreter/Interpreter.java
+++ xmlinterface/sebastian/src/de/fhtw/xgl/interpreter/Interpreter.java
@@ -4,10 +4,30 @@
  */
 package de.fhtw.xgl.interpreter;
 
-import org.w3c.dom.Document; 
+import org.w3c.dom.Document;
+
+import de.fhtw.xgl.interpreter.CallbackHandler;
 
 /**
- * @author Administrator
+ * The Interpreter is the core-class of the XML-gui.
+ * 
+ * An interpreter is used to transform an XML-structure into a GUI.<br>
+ * To read an XML-GUI-file and transform it into a GUI do the following:
+ * 
+ * <pre><code>
+	FileInputStream reader = new FileInputStream("gui.xml");
+	
+	SwingInterpreter interpreter = new SwingInterpreter(new ExampleCallbackHandler());
+
+	DocumentBuilderFactory dbFactory = 
+		DocumentBuilderFactory.newInstance();
+	dbFactory.setNamespaceAware(true);
+	DocumentBuilder builder = dbFactory.newDocumentBuilder();
+	Document document = builder.parse(reader);
+	interpreter.parseDocument(document);
+ * </code></pre>
+ * 
+ * @author Sebastian Heide
  *
  */
 public interface Interpreter
@@ -16,7 +36,7 @@ public interface Interpreter
 	/**
 	 * Represents the root-element of the xgl-language.
 	 */	
-	public static final String XML_ROOT_ELEMENT = "xgl-gui";
+	public static final String XML_ROOT_ELEMENT = "xmlgui";
 	/**
 	 * Represents the <code>widget</code>-tag of the xml-file.
 	 */
@@ -66,8 +86,54 @@ public interface Interpreter
 	 */
 	public static final String WIDGET_TYPE_CONTAINER = "container";
 	
-	public void sendCallback(Callback callback);
+	/**
+	 * This method is called by <code>Widget<code>-objects to send an event to the <code>CallbackHandler</code>.
+	 * 
+	 * @param widget
+	 */
+	public void handleEvent(Widget widget);
 
+	/**
+	 * Parses the given XML-document-structure and builds the Gui-structure.
+	 * 
+	 * The document must be a valid XML-document and a valid GUI-structure.
+	 * A Window will be created after the process and the GUI becomes visible.
+	 * 
+	 * @param doc XML-document with the GUI-structure
+	 * @throws XglInterpreterException
+	 */
 	public void parseDocument(Document doc) throws XglInterpreterException;
+	
+	/**
+	 * Sets the CallbackHandler for this Interpreter (of not done through the constructor).
+	 * 
+	 * @param cbh CallbackHandler
+	 */
+	public void setCallbackHandler(CallbackHandler cbh);
+
+	/**
+	 * Returns the CallbackHandler connected with this Interpreter.
+	 * 
+	 * @return CallbackHandler that handles GUI-events
+	 */
+	public CallbackHandler getCallbackHandler();
+	
+	/**
+	 * Returns the <code>Widget</code>-object with the specified ID.
+	 * 
+	 * @param Id ID of the Widget 
+	 * @return the Widget-object
+	 */
+	public Widget getWidgetById(int id);
+	
+	/**
+	 * This method should not be invoked directly. It adds a widget to the Interpreter's widget-list.
+	 * 
+	 * Widgets use this method to make themself known to this Interpreter-object.
+	 * 
+	 * @param id the widget's id
+	 * @param w the widget itself
+	 */
+	public void addWidget(int id, Widget w);
 
 }
